cbd185034f6c07d680224a44085a34227368f80d
Butanediol <iam@butanediol.me>
2023-07-25 14:54:06 +0800

    feat: AppInfo conforms to Identifiable

---
+++ Sources/SnipdroidSDK/Extensions/Models/Components.Schema.AppInfo+Identifiable.swift
@@ -0,0 +1,10 @@
+//
+//  Components.Schema.AppInfo+Identifiable.swift
+//  
+//
+//  Created by Butanediol on 2023/7/25.
+//
+
+import Foundation
+
+extension Components.Schemas.AppInfo: Identifiable {}
